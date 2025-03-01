e92437375371e858552cbe0787487a353c37f3ad
Eriseldi <eribeq21@htl-shkoder.com>
2025-03-01 19:03:02 +0100

    Eriseldiii

--- src/routes/+page.server.js
+++
@@ -1,6 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { link } from '$env/static/private';
-
-export async function GET() {
-    return json({ link }); // Only accessible via server request
-}
