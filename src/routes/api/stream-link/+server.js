cd14d53c5bd2f3179433445860bd9f6c0d3ff7ab
Eriseldi <eribeq21@htl-shkoder.com>
2025-03-05 18:52:07 +0100

    Last comitttt

--- src/routes/api/stream-link/+server.js
+++ src/routes/api/stream-link/+server.js
@@ -1,33 +1,7 @@
 import { json } from '@sveltejs/kit';
-import { PRIVATE_LINK, BASIC_AUTH_PASSWORD, BASIC_AUTH_USER } from '$env/static/private';
+import { PRIVATE_LINK } from '$env/static/private';
 
-async function authenticate(request) {
-    const auth = request.headers.get('authorization');
-    if (!auth) {
-        return new Response(null, {
-            status: 401,
-            headers: { 'www-authenticate': 'Basic realm="Protected Stream"' }
-        });
-    }
-
-    const base64Credentials = auth.split(' ')[1];
-    const credentials = atob(base64Credentials);
-    const [username, password] = credentials.split(':');
-
-    if (username !== BASIC_AUTH_USER || password !== BASIC_AUTH_PASSWORD) {
-        return new Response(JSON.stringify({ message: 'Access denied' }), {
-            status: 401,
-            headers: { 'www-authenticate': 'Basic realm="Protected Stream"' }
-        });
-    }
-
-    return null; // Authentication successful
-}
-
-export async function GET({ request }) {
-    const authResponse = await authenticate(request);
-    if (authResponse) return authResponse; // Return 401 if authentication fails
-
+export async function GET() {
     if (!PRIVATE_LINK) {
         console.error("❌ Error: Stream link is missing in environment variables!");
         return json({ error: "Stream link is missing" }, { status: 500 });
